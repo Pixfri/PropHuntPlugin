55659eabea50e1bec899d9125da4bea39a504eb3
Jean <jean.letessier@protonmail.com>
2025-01-11 21:58:31 +0100

    Project setup.

---
+++ src/main/java/fr/pixfri/prophunt/PropHunt.java
@@ -0,0 +1,11 @@
+package fr.pixfri.prophunt;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PropHunt extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("[PropHunt] Plugin enabled");
+    }
+}
